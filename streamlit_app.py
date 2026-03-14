e22c043a163648d8dafae88e5eddd57e5a3b349d
ez285 <ezervas.zemcogroup@gmail.com>
2026-03-14 22:51:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Sequence, Any
 if TYPE_CHECKING:
     from sqlalchemy import Engine
 
 import streamlit as st
-from sqlalchemy import text
+from sqlalchemy import text, Row
 from database import Database
 from datetime import date
 
@@ -19,7 +19,11 @@ class StreamlitMode:
 
 def ShowDate() -> None:
     st.session_state.setdefault('selectedDate', date.today())
-    st.date_input('Date', label_visibility='visible', format='DD/MM/YYYY', key='selectedDate')
+    left, right = st.columns([8, 1], vertical_alignment='bottom')
+    with left:
+        st.date_input('Date', label_visibility='visible', format='DD/MM/YYYY', key='selectedDate')
+    with right:
+        st.button('Show all for this date', use_container_width=True, on_click=lambda: setattr(st.session_state, 'Mode', StreamlitMode.FullList))
 
 def ShowCompany() -> None:
     sql = (
@@ -30,33 +34,34 @@ def ShowCompany() -> None:
     company_names = [f'{itm.entity_id} | {itm.property_value}' for itm in db.query(sql)]
     st.selectbox('Company name', company_names + ['Add New'], label_visibility='visible', key = 'selectedCompany')
 
+@st.cache_data(ttl=60)
+def _get_personnel_for_company(company_name:str) -> Sequence[Row[Any]]:
+    sql = (
+        "WITH "
+            "people AS (SELECT entity_id FROM entities WHERE entity_type = 'Person'), "
+            "company AS (SELECT entity_id FROM entities WHERE entity_type = 'Company'), "
+            "selectedCompany AS (SELECT company.entity_id FROM company "
+                "LEFT JOIN properties ON company.entity_id = properties.entity_id "
+                "WHERE property_name = 'Name' AND property_value = :selectedCompany), "
+            "selectedCompanyPersonellIDS AS (SELECT people.entity_id FROM people "
+                "LEFT JOIN properties ON people.entity_id = properties.entity_id "
+                "WHERE property_name = 'Company' AND property_value::INTEGER = (SELECT entity_id FROM selectedCompany)), "
+            "selectedCompanyPersonellNames AS (SELECT scp.entity_id, property_name,property_value FROM selectedCompanyPersonellIDS AS scp "
+                "LEFT JOIN properties ON scp.entity_id = properties.entity_id "
+                "WHERE property_name = 'First Name' OR property_name = 'Last Name') "
+        "SELECT entity_id, "
+            "MAX(CASE WHEN property_name = 'First Name' THEN property_value END) AS \"first_name\", "
+            "MAX(CASE WHEN property_name = 'Last Name' THEN property_value END) AS \"last_name\" "
+            "FROM selectedCompanyPersonellNames "
+            "GROUP BY entity_id"
+    )
+    params = {'selectedCompany': company_name}
+    return db.query(sql, params)
+
 def ShowExistingPersonell() -> None:
     company_name = st.session_state.selectedCompany.split('|')[1].strip()
-    if st.session_state.lastQueriedCompanyPersonnel != company_name or len(st.session_state.existingPersonell) == 0:
-        sql = (
-            "WITH "
-                "people AS (SELECT entity_id FROM entities WHERE entity_type = 'Person'), "
-                "company AS (SELECT entity_id FROM entities WHERE entity_type = 'Company'), "
-                "selectedCompany AS (SELECT company.entity_id FROM company "
-                    "LEFT JOIN properties ON company.entity_id = properties.entity_id "
-                    "WHERE property_name = 'Name' AND property_value = :selectedCompany), "
-                "selectedCompanyPersonellIDS AS (SELECT people.entity_id FROM people "
-                    "LEFT JOIN properties ON people.entity_id = properties.entity_id "
-                    "WHERE property_name = 'Company' AND property_value::INTEGER = (SELECT entity_id FROM selectedCompany)), "
-                "selectedCompanyPersonellNames AS (SELECT scp.entity_id, property_name,property_value FROM selectedCompanyPersonellIDS AS scp "
-                    "LEFT JOIN properties ON scp.entity_id = properties.entity_id "
-                    "WHERE property_name = 'First Name' OR property_name = 'Last Name') "
-            "SELECT entity_id, "
-                "MAX(CASE WHEN property_name = 'First Name' THEN property_value END) AS \"first_name\", "
-                "MAX(CASE WHEN property_name = 'Last Name' THEN property_value END) AS \"last_name\" "
-                "FROM selectedCompanyPersonellNames "
-                "GROUP BY entity_id"
-        )
-        params = {'selectedCompany': company_name}
-        st.session_state.existingPersonell = db.query(sql, params)
-        st.session_state.lastQueriedCompanyPersonnel = company_name
+    st.session_state.existingPersonell = _get_personnel_for_company(company_name)
 
-    
     selected = []
     for person in st.session_state.existingPersonell:
         if st.checkbox(f'{person.entity_id} | {person.first_name} {person.last_name}', key=f'person_{person.entity_id}'):
@@ -90,7 +95,6 @@ def AddNewCompany() -> None:
     db.execute(sql, params)
     st.session_state.selectedCompany = f'{company_id} | {st.session_state.newCompany}'
     st.session_state.newCompany = ''
-    st.session_state.lastQueriedCompanyPersonnel = ''
     st.session_state.Mode = StreamlitMode.NameInputStandard
 
 def ShowNewCompany() -> None:
@@ -111,7 +115,8 @@ def Submit() -> None:
                 "((SELECT entity_id FROM personID), 'First Name', :first_name, 'text'), "
                 "((SELECT entity_id FROM personID), 'Last Name', :last_name, 'text'), "
                 "((SELECT entity_id FROM personID), 'Position', '', 'text'), "
-                "((SELECT entity_id FROM personID), 'Signed H&S', 'FALSE', 'text')) "
+                "((SELECT entity_id FROM personID), 'Signed H&S', 'FALSE', 'text'), "
+                "((SELECT entity_id FROM personID), 'Documented', 'FALSE', 'text')) "
         "SELECT entity_id FROM personID"
     )
     # Log their presence
@@ -150,6 +155,34 @@ def Submit() -> None:
     for var in st.session_state.keys():
         if isinstance(var, str) and var.startswith('person_'):
             st.session_state[var] = False
+    
+    _get_personnel_for_company.clear()
+
+def ShowAll() -> None:
+    sql = (
+        "WITH "
+            "dpIDs AS (SELECT entity_id FROM entities WHERE entity_type = 'Daily Presence'), "
+            "dpIDsDate AS (SELECT dpIDs.entity_id FROM dpIDs LEFT JOIN properties ON dpIDs.entity_id = properties.entity_id WHERE property_name = 'Date' and property_value = :selected_date), "
+            "peopleIDsDate AS (SELECT property_value::INTEGER FROM dpIDsDate LEFT JOIN properties ON dpIDsDate.entity_id = properties.entity_id WHERE property_name = 'Person'), "
+            "people AS (SELECT entity_id, property_name, properties.property_value, value_type FROM peopleIDsDate LEFT JOIN properties ON peopleIDsDate.property_value = properties.entity_id), "
+            "peoplePivot AS (SELECT "
+                    "entity_id AS \"Person ID\", "
+                    "MAX(CASE WHEN property_name = 'First Name' THEN property_value END) AS \"First Name\", "
+                    "MAX(CASE WHEN property_name = 'Last Name' THEN property_value END) AS \"Last Name\", "
+                    "MAX(CASE WHEN property_name = 'Company' THEN property_value END) AS \"Company\", "
+                    "MAX(CASE WHEN property_name = 'Position' THEN property_value END) AS \"Position\", "
+                    "MAX(CASE WHEN property_name = 'Signed H&S' THEN property_value END) AS \"Signed H&S\", "
+                    "MAX(CASE WHEN property_name = 'Documented' THEN property_value END) AS \"Documented\" "
+                "FROM people "
+                "GROUP BY entity_id) "
+        "SELECT \"Person ID\", \"First Name\", \"Last Name\", property_value AS \"Company\", \"Position\", \"Signed H&S\", \"Documented\" "
+        "FROM peoplePivot LEFT JOIN properties ON peoplePivot.\"Company\" = properties.entity_id where property_name = 'Name'"
+    )
+    params = {'selected_date': st.session_state.selectedDate.isoformat()}
+
+    people = db.query(sql, params)
+    st.button('Back', on_click=lambda: setattr(st.session_state,'Mode', StreamlitMode.NameInputStandard))
+    st.dataframe(people)
 
 def ShowSubmitButton() -> None:
     st.button('Submit', on_click=Submit)
@@ -167,15 +200,12 @@ def SetUpStateVariables() -> None:
         st.session_state.newPersonell = []
     if 'newCompanyPersonell' not in st.session_state:
         st.session_state.newCompanyPersonell = []
-    if 'lastQueriedCompanyPersonnel' not in st.session_state:
-        st.session_state.lastQueriedCompanyPersonnel = ''
 
 def ResetStateVariables() -> None:
     st.session_state.existingPersonell = []
     st.session_state.selectedExistingPeople = None
     st.session_state.newPersonell = []
     st.session_state.newCompanyPersonell = []
-    st.session_state.lastQueriedCompanyPersonnel = ''
 
 db = _get_database()
 # First, set up all state variable, including modes
@@ -183,6 +213,7 @@ SetUpStateVariables()
 # Secondly, reset state variable, except for modes, if mode changes
 if st.session_state.Mode != st.session_state.PreviousMode:
     ResetStateVariables()
+    st.session_state.PreviousMode = st.session_state.Mode
 # Plot title
 st.title('Presence log')
 
@@ -203,4 +234,4 @@ elif st.session_state.Mode == StreamlitMode.NameInputNewCompany:
         st.rerun()
     ShowNewCompany()
 elif st.session_state.Mode == StreamlitMode.FullList:
-    pass
+    ShowAll()
